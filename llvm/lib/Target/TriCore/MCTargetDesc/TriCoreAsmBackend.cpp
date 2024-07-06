8f5ab2485f58ae97fd8210e5fa23983083aea3c2
Alexandru Agape <alexandru.agape@student.tugraz.at>
2024-07-06 13:22:12 +0200

    Add fixups handling
    Fixups are a crucial component in the generation of relocatable and
    linkable code, allowing compilers and linkers to produce executable
    binaries that can correctly access symbols regardless of where they are
    located in memory.

--- llvm/lib/Target/TriCore/MCTargetDesc/TriCoreAsmBackend.cpp
+++ llvm/lib/Target/TriCore/MCTargetDesc/TriCoreAsmBackend.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "MCTargetDesc/TriCoreMCTargetDesc.h"
+#include "MCTargetDesc/TriCoreFixupKinds.h"
 #include "llvm/MC/MCAsmBackend.h"
 #include "llvm/MC/MCAssembler.h"
 #include "llvm/MC/MCDirectives.h"
@@ -55,14 +56,56 @@ public:
 
 bool TriCoreAsmBackend::writeNopData(raw_ostream &OS, uint64_t Count,
                                    const MCSubtargetInfo *STI) const {
+  if (Count == 0) {
+    return true;
+  }
   return false;
+}
+
+static unsigned adjustFixupValue(const MCFixup &Fixup, uint64_t Value,
+                                 MCContext *Ctx = NULL) {
+  unsigned Kind = Fixup.getKind();
+  switch (Kind) {
+  default:
+    llvm_unreachable("Unknown fixup kind!");
+  case TRICORE::fixup_call:
+    return Value & 0xffffff;
+  case TRICORE::fixup_leg_mov_hi16_pcrel:
+    Value >>= 16;
+    // Intentional fall-through
+  case TRICORE::fixup_leg_mov_lo16_pcrel:
+    unsigned Hi4  = (Value & 0xF000) >> 12;
+    unsigned Lo12 = Value & 0x0FFF;
+    // inst{19-16} = Hi4;
+    // inst{11-0} = Lo12;
+    Value = (Hi4 << 16) | (Lo12);
+    return Value;
+  }
+  return Value;
 }
 
 void TriCoreAsmBackend::applyFixup(const MCAssembler &Asm, const MCFixup &Fixup,
                                  const MCValue &Target,
                                  MutableArrayRef<char> Data, uint64_t Value,
-                                 bool /*IsResolved*/,
-                                 const MCSubtargetInfo * /*STI*/) const {}
+                                 bool IsResolved,
+                                 const MCSubtargetInfo * /*STI*/) const 
+{
+  unsigned NumBytes = 4;
+  Value = adjustFixupValue(Fixup, Value);
+  if (!Value) {
+    return; // Doesn't change encoding.
+  }
+
+  unsigned Offset = Fixup.getOffset();
+  assert(Offset + NumBytes <= Data.size() && "Invalid fixup offset!");
+
+  // For each byte of the fragment that the fixup touches, mask in the bits from
+  // the fixup value. The Value has been "split up" into the appropriate
+  // bitfields above.
+  for (unsigned i = 0; i != NumBytes; ++i) {
+    Data[Offset + i] |= uint8_t((Value >> (i * 8)) & 0xff);
+  }
+}
 
 std::unique_ptr<MCObjectTargetWriter>
 TriCoreAsmBackend::createObjectTargetWriter() const {
@@ -71,7 +114,23 @@ TriCoreAsmBackend::createObjectTargetWriter() const {
 
 const MCFixupKindInfo &
 TriCoreAsmBackend::getFixupKindInfo(MCFixupKind Kind) const {
-  return FixupKindInfo;
+  const static MCFixupKindInfo Infos[TRICORE::NumTargetFixupKinds] = {
+      // This table *must* be in the order that the fixup_* kinds are defined in
+      // TriCoreFixupKinds.h.
+      //
+      // { Name, Offset (bits), Size (bits), Flags }
+      { "fixup_leg_mov_hi16_pcrel", 0, 32, MCFixupKindInfo::FKF_IsPCRel },
+      { "fixup_leg_mov_lo16_pcrel", 0, 32, MCFixupKindInfo::FKF_IsPCRel },
+      { "fixup_call", 0, 24, 0 },
+    };
+
+    if (Kind < FirstTargetFixupKind) {
+      return MCAsmBackend::getFixupKindInfo(Kind);
+    }
+
+    assert(unsigned(Kind - FirstTargetFixupKind) < getNumFixupKinds() &&
+           "Invalid kind!");
+    return Infos[Kind - FirstTargetFixupKind];
 }
 
 } // namespace
